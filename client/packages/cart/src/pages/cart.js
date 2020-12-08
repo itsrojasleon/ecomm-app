9b5d114557b7143a5f5f2edb701c2549a665e2fc
rojasleon <rojasleon.dev@gmail.com>
2020-12-08 15:03:39 -0600

    total

--- client/packages/cart/src/pages/cart.js
+++ client/packages/cart/src/pages/cart.js
@@ -2,6 +2,17 @@ import React, { useContext, useEffect, useState } from 'react';
 import ProductItem from '../components/cart-item';
 import { Context } from '../context/cart-context';
 
+// pass an array
+const sum = (arr) => {
+  const result = arr.reduce(
+    (previousValue, currentValue) =>
+      previousValue + currentValue.product.price * currentValue.quantity,
+    0
+  );
+
+  return result;
+};
+
 const Cart = () => {
   const { state, fetchItems } = useContext(Context);
 
@@ -17,7 +28,7 @@ const Cart = () => {
       {state.items.map((item) => (
         <ProductItem key={item.id} {...item} />
       ))}
-      <div>Total: $ {}</div>
+      <div>Total: $ {sum(state.items)}</div>
     </div>
   );
 };
